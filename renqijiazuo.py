9a16525353a9695d2f198b850ec9700f4e7d50b7
GingerCP <jingerfish.stories@gmail.com>
2025-03-04 12:48:28 -0500

    Update renqijiazuo.py

--- renqijiazuo.py
+++ renqijiazuo.py
@@ -1,8 +1,9 @@
 import requests
 import json
-
+from datetime import datetime
 # URL of the webpage
 novels = []
+fetch_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 for page in range(0,1):
   url = 'https://www.gongzicp.com/webapi/home/recommend?page_id=0&tid=0&page=2'
   headers = {"User-Agent": "Mozilla/5.0"}
@@ -22,6 +23,7 @@ for page in range(0,1):
         "Name": novel["novel_name"],
         "Popularity": novel["novel_allpopu"],
         "Collection": novel["novel_allcoll"],
+        "Timestamp": fetch_time  # Store fetch time
     })
     print(novel['novel_name'], novel['novel_id'], novel['novel_allpopu'], novel['novel_allpopu'])
     with open('renqijiazuo.json', "w", encoding="utf-8") as file:
