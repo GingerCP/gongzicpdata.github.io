3ac1e2577e96a114794306caa32b6274605f5d97
GingerCP <jingerfish.stories@gmail.com>
2025-03-04 13:07:57 -0500

    Update renqijiazuo.py

--- renqijiazuo.py
+++ renqijiazuo.py
@@ -1,9 +1,13 @@
 import requests
 import json
-from datetime import datetime
-# URL of the webpage
-novels = []
-fetch_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+from datetime import datetime, timedelta
+
+# Convert UTC time to China Standard Time (CST)
+def get_china_time():
+    return (datetime.utcnow() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
+
+fetch_time = get_china_time()
+
 for page in range(0,1):
   url = 'https://www.gongzicp.com/webapi/home/recommend?page_id=0&tid=0&page=2'
   headers = {"User-Agent": "Mozilla/5.0"}
