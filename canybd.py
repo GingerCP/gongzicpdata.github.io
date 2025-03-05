d28fb5d88bc97509769d2680b75311aa606ecc99
GingerCP <jingerfish.stories@gmail.com>
2025-03-05 01:39:32 -0500

    Update canybd.py

--- canybd.py
+++ canybd.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 def get_china_time():
     return (datetime.utcnow() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
 headers = {"User-Agent": "Mozilla/5.0"}    
+novel_rank = []
 identifiers = ['xsjxyc', 'ybzsyc', 'yccqs', 'ycgd' ,'ycxdds','ychx','ycxy', 'ycjk','qgrzyc','bzxbyc','ycxdzh','ycxdqc']
 names = ['新书精选','鱼宝赞赏','长篇热读','纯爱古代','纯爱都市','纯爱幻想','纯爱悬疑','纯爱架空','勤更热追','本周寻宝','纯爱综合','纯爱青春']
 for identifier in identifiers:
