37ab85e0dad39ec10c6bffc9f50385014b3b42e5
GingerCP <jingerfish.stories@gmail.com>
2025-03-06 18:26:49 -0500

    Update chunaitaojin_increase.py

--- chunaitaojin_increase.py
+++ chunaitaojin_increase.py
@@ -1,6 +1,12 @@
 import requests
 import json
 from datetime import datetime, timedelta
+# Convert UTC time to China Standard Time (CST)
+def get_china_time():
+    return (datetime.utcnow() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
+
+fetch_time = get_china_time()
+
 # json_name_list = ['chunaitaojin','benzhouqiangtui','bidu_weekly','bobaow_weekly','chunaishouye','renqijiazuo','sybd','sygd','syhx','syjk','syxd','syxy','xinxiuqiangtui']
 # data_old =json.loads('chunaitaojin.json')
 with open('chunaitaojin.json', 'r', encoding='utf-8') as f:
